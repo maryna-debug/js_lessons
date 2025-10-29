f11e7aa4e338a7fb0522285d8f5da425f441da37
maryna-debug <valentir45@gmail.com>
2025-10-29 19:49:48 +0100

    Added code for JS homework 4

---
+++ homework4.js
@@ -0,0 +1,54 @@
+// homework4.js - JS homework #4 with step-by-step comments
+
+// 1
+// 'number' + 3 + 3 -> 'number33'
+console.log('1:', 'number' + 3 + 3);
+
+// 2
+// null + 3 -> 3 (null -> 0)
+console.log('2:', null + 3);
+
+// 3
+// 5 && "qwerty" -> 'qwerty'
+console.log('3:', 5 && "qwerty");
+
+// 4
+// +'40' + +'2' + "hillel" -> '42hillel'
+console.log('4:', +'40' + +'2' + "hillel");
+
+// 5
+// '10' - 5 === 6 -> false
+console.log('5:', '10' - 5 === 6);
+
+// 6
+// true + false -> 1
+console.log('6:', true + false);
+
+// 7
+// '4px' - 3 -> NaN
+console.log('7:', '4px' - 3);
+
+// 8
+// '4' - 3 -> 1
+console.log('8:', '4' - 3);
+
+// 9
+// '6' + 3 ** 0 -> '61'
+console.log('9:', '6' + 3 ** 0);
+
+// 10
+// 12 / '6' -> 2
+console.log('10:', 12 / '6');
+
+// 11
+// '10' + (5 === 6) -> '10false'
+console.log('11:', '10' + (5 === 6));
+
+// 12
+// null == '' -> false
+console.log('12:', null == '');
+
+// 13
+// 3 ** (9 / 3) -> 27
+console
+
