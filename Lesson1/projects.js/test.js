fb98f2b60c1643c8855c9c30f8bea0bd142b597f
maryna-debug <valentir45@gmail.com>
2025-11-07 18:27:14 +0100

    cleanup: keep only homework4

--- Lesson1/projects.js/test.js
+++
@@ -1,7 +0,0 @@
-node -v
- 
-function squareAreaCalculation() {
-    let a = prompt("Ввести сторону квадрату"); // Нечиста функція через побічний ефект
-    let result = a * a;
-    console.log(result);  // Нечиста функція через побічний ефект
-}
